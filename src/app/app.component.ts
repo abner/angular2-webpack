bac3a6ccd4c391279a94f58200bfe8e89270c129
Abner Oliveira <abner.silva@gmail.com>
2016-06-13 07:35:30 -0300

    using material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,20 @@ import { AboutComponent } from './about';
 import {BoxComponent} from './layout/box.component.ts';
 import {ToolbarComponent} from './layout/toolbar.component';
 
+
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import {MdInput} from '@angular2-material/input';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton, MdRadioGroup, MdRadioDispatcher} from '@angular2-material/radio';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+
+
 import '../style/app.scss';
 
 /*
@@ -16,8 +30,22 @@ import '../style/app.scss';
  */
 @Component({
   selector: 'aso-app', // <my-app></my-app>
-  providers: [ApiService],
-  directives: [BoxComponent, ToolbarComponent, ...ROUTER_DIRECTIVES],
+  providers: [MdIconRegistry, MdRadioDispatcher, ApiService],
+  directives: [
+    BoxComponent,
+    ToolbarComponent,
+    ...ROUTER_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MD_LIST_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdToolbar,
+    MdButton,
+    MdInput,
+    MdCheckbox,
+    MdRadioGroup,
+    MdRadioButton,
+    MdIcon],
+
   template: require('./app.component.html'),
   styles: [require('./app.component.scss')],
 })
@@ -26,6 +54,20 @@ import '../style/app.scss';
   { path: '/About', component: AboutComponent, name: 'About' }
 ])
 export class AppComponent {
+
+  formShowing: boolean = false;
+  views: Object[] = [
+    {
+      name: "My Account",
+      description: "Edit my account information",
+      icon: "assignment ind"
+    },
+    {
+      name: "Potential dates",
+      description: "Find your soulmate!",
+      icon: "pets"
+    }
+  ];
   url = 'https://github.com/preboot/angular2-webpack';
 
   constructor(private api: ApiService) {
